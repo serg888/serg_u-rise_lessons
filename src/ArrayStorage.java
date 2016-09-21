f8e61e11bb70e6df25b1d6da5c1ccc111c52dcd5
Сергей <serg888@mail.ru>
2016-09-21 20:33:08 +0700

    Revert: Implements all method in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,7 +9,9 @@ import java.util.List;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    public final int arrayLenght=storage.length;
     private int size=0;
+
 
     void clear() {
         storage=new Resume[10000];
@@ -17,9 +19,15 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
+        //проверка есть ли уже такой элемент в массиве
+        if(get(r.uuid)==null)
+        {
+            //сравнение с переменной (на непревышение максимальной длины массива)
+            if(size<arrayLenght) storage[size++]=r; else
+            System.out.println("Error: Not enough space");
+        } else
+            System.out.println("Error: this resume is already in array" );
 
-        if(size<storage.length) storage[size++]=r; else
-            System.out.println("Not enough space");
     }
 
     Resume get(String uuid) {
@@ -34,21 +42,26 @@ public class ArrayStorage {
     void delete(String uuid) {
         boolean isFind=false;
         for(int i=0;i<size;i++){
-            if(storage[i]!=null&&!isFind)
-                if (storage[i].uuid.equals(uuid))
+                if (storage[i].uuid.equals(uuid)){
                     isFind=true;
-            if(isFind){
-                if(i<size-1)storage[i]=storage[i+1];else
-                    storage[i]=null;
-            }
+                    storage[i]=storage[size-1];
+                    storage[size-1]=null;
+                    break;
+                }
+
         }
-        if(isFind)size--;
+        if(isFind)size--; else
+            System.out.println("Error: resume not found");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {        return Arrays.copyOfRange(storage,0,size);    }
+    Resume[] getAll() {
+        Resume[] r1=new Resume[size];
+        for(int i=0;i<size;i++)
+            r1[i]=storage[i];
+        return r1;    }
 
     int size() {
         return size;
